17ef05f646c0ff244d69df22b9fa78b10dca9b3a
AnastasiiaV0304 <volosina608@gmail.com>
2024-05-13 14:15:35 +0300

    first commit

---
+++ src/basic/3.ts
@@ -0,0 +1,10 @@
+let mixedType: string | number;
+
+mixedType = 'String';
+mixedType = 10;
+
+type enableOrDisable = 'enable' | 'disable';
+let answer: enableOrDisable;
+
+answer = 'enable';
+answer = 'disable';
